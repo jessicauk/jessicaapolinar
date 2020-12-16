7b5c24d40e344ed69cddc39e9b4bdb4633faf44b
Jessica Apolinar <jessica.uk220691@gmail.com>
2020-12-16 00:22:23 -0600

    skills-section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
 import About from './components/About/About'
+import Services from './components/Services/Services'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Banner />
       <Header />
       <About />
+      <Services />
     </Fragment>
   )
 }
