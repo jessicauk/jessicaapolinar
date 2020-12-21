7d21d09ea80f96fdf3a4cf07819732a1c9e1e731
Jessica Apolinar <jessica.uk220691@gmail.com>
2020-12-20 22:26:54 -0600

    feat: update banner image

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 const path = require("path");
+
+const PUBLIC_URL = process.env.PUBLIC_URL || '/';
 
 console.log("__dirname",__dirname )
 
@@ -8,14 +12,18 @@ module.exports = {
   entry: { index: path.resolve(__dirname, "src", "index.js") },
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "[name].bundle.js", 
+    filename: "[name].bundle.[hash].js",
+    publicPath: PUBLIC_URL, 
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
       title: 'Output Management'
-    })
+    }),
+    new webpack.DefinePlugin({
+      PUBLIC_URL: JSON.stringify(PUBLIC_URL),
+    }),
   ],
   module: {
     rules: [
@@ -29,12 +37,41 @@ module.exports = {
         use: ["babel-loader"]
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        exclude: /node_modules/,
         use: ["file-loader"],
       },
     ]
   },
   optimization: {
-    splitChunks: { chunks: "all" }
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`
+    ],
+    splitChunks: { 
+    // chunks: "all", 
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
   }
 };
