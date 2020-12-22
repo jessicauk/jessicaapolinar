84f06e1e85c31762d2ff42424e9af232c1005a64
Jessica Apolinar <jessica.uk220691@gmail.com>
2020-12-22 08:23:58 -0600

    feat: cards work

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
 import About from './components/About/About'
 import Services from './components/Services/Services'
+import Work from './components/Work/Work'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <About />
       <Services />
+      <Work />
     </Fragment>
   )
 }
